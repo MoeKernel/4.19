8449fbc2f3771f6bbeaa4c0db1ebf60f48d501c3
明 <akariondev@gmail.com>
2024-06-25 20:47:23 +0000

    build: adapting for kernel 4.19
    Signed-off-by: 明 <akariondev@gmail.com>

---
+++ moe.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+# Compile script for MoeKernel
+# Copyright (C) 2020-2021 Adithya R.
+
+SECONDS=0
+ZIPNAME="MoeKSU-$(date '+%Y%m%d').zip"
+TC_DIR="$HOME/tc/clang-19.0.0"
+GCC_64_DIR="$HOME/tc/aarch64-linux-android-15.0"
+GCC_32_DIR="$HOME/tc/arm-linux-androideabi-15.0"
+AK3_DIR="$HOME/android/AnyKernel3"
+DEFCONFIG="vendor/moe_defconfig"
+
+export PATH="$TC_DIR/bin:$PATH"
+
+export KBUILD_BUILD_USER=Moe
+export KBUILD_BUILD_HOST=Nyan
+
+if ! [ -d "${TC_DIR}" ]; then
+    echo "Clang not found! Cloning to ${TC_DIR}..."
+    if ! git clone --depth=1 https://gitlab.com/moehacker/clang-r498229b ${TC_DIR}; then
+        echo "Cloning failed! Aborting..."
+        exit 1
+    fi
+fi
+
+if ! [ -d "${GCC_64_DIR}" ]; then
+    echo "gcc not found! Cloning to ${GCC_64_DIR}..."
+    if ! git clone --depth=1 -b 15 https://github.com/whyakari/aarch64-zyc-linux-gnu ${GCC_64_DIR}; then
+        echo "Cloning failed! Aborting..."
+        exit 1
+    fi
+fi
+
+if ! [ -d "${GCC_32_DIR}" ]; then
+    echo "gcc_32 not found! Cloning to ${GCC_32_DIR}..."
+    if ! git clone --depth=1 -b 15 https://github.com/whyakari/arm-zyc-linux-gnueabi ${GCC_32_DIR}; then
+        echo "Cloning failed! Aborting..."
+        exit 1
+    fi
+fi
+
+if [[ $1 = "-r" || $1 = "--regen" ]]; then
+    make O=out ARCH=arm64 $DEFCONFIG savedefconfig
+    cp out/defconfig arch/arm64/configs/$DEFCONFIG
+    exit
+fi
+
+if [[ $1 = "-c" || $1 = "--clean" ]]; then
+    rm -rf out
+fi
+
+if [[ $1 = "-m" || $1 = "--menu" ]]; then
+    mkdir -p out
+    make O=out ARCH=arm64 $DEFCONFIG menuconfig
+elif [[ $1 = "menu" ]]; then
+    mkdir -p out
+    make O=out ARCH=arm64 $DEFCONFIG menuconfig
+else
+    mkdir -p out
+    make O=out ARCH=arm64 $DEFCONFIG
+fi
+
+echo -e "\nStarting compilation... wait\n"
+make -j$(nproc --all) \
+    O=out \
+    ARCH=arm64 \
+    CROSS_COMPILE=$GCC_64_DIR/bin/aarch64-linux-android- \
+    CROSS_COMPILE_ARM32=$GCC_32_DIR/bin/arm-linux-androideabi- \
+    CLANG_TRIPLE=aarch64-linux-gnu- \
+    LLVM=1 \
+    LLVM_IAS=1 \
+    # Image.gz-dtb dtbo.img
+
+if [ -f "out/arch/arm64/boot/Image.gz-dtb" ] && \
+   [ -f "out/arch/arm64/boot/dtbo.img" ]; then
+    echo -e "\nKernel compiled successfully! Zipping up...\n"
+    if [ -d "$AK3_DIR" ]; then
+        cp -r $AK3_DIR AnyKernel3
+    elif ! git clone -q https://github.com/MoeKernel/AnyKernel3; then
+        echo -e "\nAnyKernel3 repo not found locally and cloning failed! Aborting..."
+        exit 1
+    fi
+    cp out/arch/arm64/boot/Image.gz-dtb AnyKernel3
+    cp out/arch/arm64/boot/dtbo.img AnyKernel3
+    rm -f *zip
+    cd AnyKernel3
+    git checkout master &> /dev/null
+    zip -r9 "../$ZIPNAME" * -x '*.git*' README.md *placeholder
+    cd ..
+    rm -rf AnyKernel3
+    rm -rf out/arch/arm64/boot
+    echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
+    echo "Zip: $ZIPNAME"
+else
+    echo -e "\nCompilation failed!"
+    exit 1
+fi
